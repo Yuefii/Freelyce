02d09f0135e8495a78ba3480b6f9769a51bf4baa
Yuefii <muffidahmadiawan31@gmail.com>
2025-08-22 10:11:06 +0700

    chore: remove go on api, and refact structue nextjs for fullstack

---
+++ app/(public)/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <>
+      <div className="h-[700px] bg-[#14BA85]">{/* CONTENT */}</div>
+    </>
+  );
+}
--- pkg/models/invoice-items.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type InvoiceItem struct {
-	ID          uint    `gorm:"primaryKey"`
-	InvoiceID   uint    `gorm:"not null"`
-	Description string  `gorm:"size:255;not null"`
-	Quantity    int     `gorm:"not null"`
-	UnitPrice   float64 `gorm:"type:decimal(10,2);not null"`
-}
