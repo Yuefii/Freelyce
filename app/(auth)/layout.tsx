b8b43a04f429e1c670e2798f1e81dacd9fca8e2f
Yuefii <muffidahmadiawan31@gmail.com>
2025-08-23 14:16:04 +0700

    chore: refact color on layouting auth

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <main>
-        <div className="min-h-screen bg-[#14BA85] flex flex-col justify-center sm:px-6 lg:px-8">
+        <div className="min-h-screen bg-background flex flex-col justify-center sm:px-6 lg:px-8">
           {children}
         </div>
       </main>
