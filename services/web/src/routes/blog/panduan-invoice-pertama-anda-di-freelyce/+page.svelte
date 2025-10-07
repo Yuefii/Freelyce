f2f1b7dc5e52efa73321255cbaca5aab630407ef
Yuefii <muffidahmadiawan31@gmail.com>
2025-10-07 14:36:17 +0700

    chore: change icon svg to icon lucide svelte

--- services/web/src/routes/blog/panduan-invoice-pertama-anda-di-freelyce/+page.svelte
+++ services/web/src/routes/blog/panduan-invoice-pertama-anda-di-freelyce/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Footer from '$lib/components/core/footer.svelte';
 	import Navbar from '$lib/components/core/navbar.svelte';
+	import { ChevronRight } from 'lucide-svelte';
 
 	const article = {
 		category: 'Panduan Pengguna',
@@ -49,19 +50,7 @@
 					>
 				</li>
 				<li>
-					<svg
-						class="h-5 w-5 flex-shrink-0 text-gray-400"
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 20 20"
-						fill="currentColor"
-						aria-hidden="true"
-					>
-						<path
-							fill-rule="evenodd"
-							d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-							clip-rule="evenodd"
-						/>
-					</svg>
+					<ChevronRight class="h-5 w-5 flex-shrink-0 text-gray-400" />
 				</li>
 				<li>
 					<a
@@ -70,19 +59,7 @@
 					>
 				</li>
 				<li>
-					<svg
-						class="h-5 w-5 flex-shrink-0 text-gray-400"
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 20 20"
-						fill="currentColor"
-						aria-hidden="true"
-					>
-						<path
-							fill-rule="evenodd"
-							d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-							clip-rule="evenodd"
-						/>
-					</svg>
+					<ChevronRight class="h-5 w-5 flex-shrink-0 text-gray-400" />
 				</li>
 				<li>
 					<span class="font-medium text-gray-700">{article.title}</span>
